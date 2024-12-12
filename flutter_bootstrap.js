452ac76761d5e79e02a7acef833ada90e663adf8
Sobitandra <kartikghosh770@gmail.com>
2024-12-12 18:24:38 +0530

    deploy4

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"a18df97ca57a249df5d8d68cd0820600223ce2
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "982098436"
+    serviceWorkerVersion: "1224922167"
   }
 });
